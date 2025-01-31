7cd6ff8e508c87d9d89138c63e1427805cb43786
HimanshuLE <Himanshu.kushwaha@leverageedu.com>
2025-01-31 11:57:00 +0530

    mobile screen

--- app/page.js
+++ app/page.js
@@ -1,24 +1,41 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
-import HomePage from "./components/HomePage"
+import HomePage from "./components/HomePage";
+import { TypeAnimation } from "react-type-animation";
 export default function Home() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const [mobile, setMobile] = useState(false);
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false)
+      setLoading(false);
+      if (window.innerWidth < 800) {
+        setMobile(true);
+      }
     }, 1500);
-  }, [])
+  }, []);
   return (
     <main>
-     { loading ?
-    <div className="video-container">
-  <video autoPlay muted loop playsInline>
-    <source src="/assets/Loading.mp4" type="video/mp4" />
-  </video>
-</div>
-     :
-      <HomePage />}
+      {!mobile ? (
+        loading ? (
+          <div className="video-container">
+            <video autoPlay muted loop playsInline>
+              <source src="/assets/Loading.mp4" type="video/mp4" />
+            </video>
+          </div>
+        ) : (
+          <HomePage />
+        )
+      ) : (
+        <div className="mobileScreen">
+          <TypeAnimation
+            sequence={["View this on desktop for better experience."]}
+            wrapper="span"
+            speed={70}
+            style={{ color: "#00FF00", fontSize: 20 }}
+          />{" "}
+        </div>
+      )}
     </main>
   );
 }
